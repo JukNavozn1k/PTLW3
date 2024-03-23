8ebbd4587a9504d4eeefbbde99b4b8d2213a892f
JukNavozn1k <JukNavozn1k>
2024-03-23 04:33:49 -0700

    Add project files.

---
+++ PTLW3/DistanceCalculator.cpp
@@ -0,0 +1,66 @@
+#include <Windows.h>
+
+WNDCLASS BaseWindow(HBRUSH BGColor, HCURSOR Cursor, HINSTANCE hInst, HICON Icon, LPCWSTR Name, WNDPROC Procedure)
+{
+	WNDCLASS BWC = { 0 };
+
+	BWC.hIcon = Icon;
+	BWC.hCursor = Cursor;
+	BWC.hInstance = hInst;
+	BWC.hIcon = Icon;
+	BWC.lpszClassName = Name;
+	BWC.hbrBackground = BGColor;
+	BWC.lpfnWndProc = Procedure;
+
+	return BWC;
+}
+
+void AdjBuilder(HWND hwnd)
+{
+	// Poopy grid
+	CreateWindowA("static", "ADJ Matrix", WS_VISIBLE | WS_CHILD, 5, 5, 100, 100, hwnd, NULL, NULL, NULL);
+
+	// for ... 
+
+	CreateWindowA("button", "Enter", WS_VISIBLE | WS_CHILD, 5, 50, 100, 50, hwnd, NULL, NULL, NULL);
+}
+
+
+LRESULT CALLBACK BaseProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp)
+{
+	// All bullshit handler
+
+	switch (msg)
+	{
+	case WM_CREATE:
+		AdjBuilder(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:return DefWindowProc(hWnd, msg, wp, lp);
+	}
+}
+
+int WINAPI WinMain(HINSTANCE hInst,HINSTANCE hPrevInst,LPSTR args, int ncmdshow)
+{
+	WNDCLASS ParentWindow = BaseWindow((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(NULL, IDI_WINLOGO),
+		L"ParentWindow", BaseProcedure);
+
+	
+	if (!RegisterClassW(&ParentWindow)) { return -1; }
+
+	MSG msg = { 0 };
+	
+
+	CreateWindow(L"ParentWindow",L"ADJ Matrix",  WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 500, 500, NULL, NULL, NULL, NULL);
+
+	while (GetMessage(&msg, NULL, NULL, NULL))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return 0;
+
+}
+
